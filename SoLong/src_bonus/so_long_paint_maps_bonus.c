59511010b1d58ee20ec4380f61dc2feade4ad2e8
Javier Roldan poces <javierroldanpoces@gmail.com>
2023-05-30 22:53:27 +0200

    Makefile + src_bonus

---
+++ SoLong/src_bonus/so_long_paint_maps_bonus.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long_paint_maps_bonus.c                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: javier <javier@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/14 11:46:31 by javier            #+#    #+#             */
+/*   Updated: 2023/05/30 22:49:32 by javier           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long_bonus.h"
+
+void	paint_maps(t_so_long *c)
+{
+	int	fil;
+	int	col;
+
+	fil = -1;
+	while (++fil < c->h)
+	{
+		col = -1;
+		while (++col < c->w)
+		{
+			mlx_image_to_window(c->mlx, c->bg, col * 64, fil * 64);
+			if (c->map[fil][col] == '1')
+				mlx_image_to_window(c->mlx, c->wall, col * 64, fil * 64);
+			else if (c->map[fil][col] == 'C')
+				mlx_image_to_window(c->mlx, c->bone, col * 64, fil * 64);
+			else if (c->map[fil][col] == 'E')
+				mlx_image_to_window(c->mlx, c->open, col * 64, fil * 64);
+		}
+	}
+}
+
+void	paint_collec(t_so_long *c)
+{
+	int	fil;
+	int	col;
+
+	fil = 0;
+	c->bone = mlx_texture_to_image(c->mlx, c->t_bone);
+	while (fil < c->h)
+	{
+		col = 0;
+		while (col < c->w)
+		{
+			if (c->map[fil][col] == 'C')
+			{	
+				mlx_image_to_window(c->mlx, c->bone, col * S, fil * S);
+			}
+			col++;
+		}
+		fil++;
+	}
+}
